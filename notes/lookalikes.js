8f5d5af45e4fb0b457278a1dcfc815f3dd12d2e5
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2020-10-29 13:05:19 -0500

    massive array of potential homophones

---
+++ notes/lookalikes.js
@@ -0,0 +1,677 @@
+module.exports = [
+  {
+    "character": " ",
+    "homoglyphs": [
+      " ",
+      " ",
+      " "
+    ]
+  },
+  {
+    "character": "!",
+    "homoglyphs": [
+      "ǃ",
+      "！"
+    ]
+  },
+  {
+    "character": "\"",
+    "homoglyphs": [
+      "״",
+      "″",
+      "＂"
+    ]
+  },
+  {
+    "character": "$",
+    "homoglyphs": [
+      "＄"
+    ]
+  },
+  {
+    "character": "%",
+    "homoglyphs": [
+      "％"
+    ]
+  },
+  {
+    "character": "&",
+    "homoglyphs": [
+      "＆"
+    ]
+  },
+  {
+    "character": "'",
+    "homoglyphs": [
+      "＇"
+    ]
+  },
+  {
+    "character": "(",
+    "homoglyphs": [
+      "﹝",
+      "（"
+    ]
+  },
+  {
+    "character": ")",
+    "homoglyphs": [
+      "﹞",
+      "）"
+    ]
+  },
+  {
+    "character": "*",
+    "homoglyphs": [
+      "⁎"
+    ]
+  },
+  {
+    "character": "+",
+    "homoglyphs": [
+      "＋"
+    ]
+  },
+  {
+    "character": ",",
+    "homoglyphs": [
+      "‚",
+      "，"
+    ]
+  },
+  {
+    "character": "-",
+    "homoglyphs": [
+      "‐"
+    ]
+  },
+  {
+    "character": ".",
+    "homoglyphs": [
+      "٠",
+      "۔",
+      "܁",
+      "܂",
+      "․",
+      "‧",
+      "．"
+    ]
+  },
+  {
+    "character": "/",
+    "homoglyphs": [
+      "̸",
+      "⁄",
+      "∕",
+      "╱"
+    ]
+  },
+  {
+    "character": "1",
+    "homoglyphs": [
+      "１"
+    ]
+  },
+  {
+    "character": "2",
+    "homoglyphs": [
+      "２"
+    ]
+  },
+  {
+    "character": "3",
+    "homoglyphs": [
+      "３"
+    ]
+  },
+  {
+    "character": "4",
+    "homoglyphs": [
+      "４"
+    ]
+  },
+  {
+    "character": "5",
+    "homoglyphs": [
+      "５"
+    ]
+  },
+  {
+    "character": "6",
+    "homoglyphs": [
+      "６"
+    ]
+  },
+  {
+    "character": "8",
+    "homoglyphs": [
+      "８"
+    ]
+  },
+  {
+    "character": "9",
+    "homoglyphs": [
+      "９"
+    ]
+  },
+  {
+    "character": ":",
+    "homoglyphs": [
+      "։",
+      "∶",
+      "꞉",
+      "："
+    ]
+  },
+  {
+    "character": ";",
+    "homoglyphs": [
+      ";",
+      "；"
+    ]
+  },
+  {
+    "character": "<",
+    "homoglyphs": [
+      "‹",
+      "＜"
+    ]
+  },
+  {
+    "character": ">",
+    "homoglyphs": [
+      "›",
+      "＞"
+    ]
+  },
+  {
+    "character": "?",
+    "homoglyphs": [
+      "？"
+    ]
+  },
+  {
+    "character": "@",
+    "homoglyphs": [
+      "＠"
+    ]
+  },
+  {
+    "character": "[",
+    "homoglyphs": [
+      "［"
+    ]
+  },
+  {
+    "character": "]",
+    "homoglyphs": [
+      "］"
+    ]
+  },
+  {
+    "character": "^",
+    "homoglyphs": [
+      "＾"
+    ]
+  },
+  {
+    "character": "_",
+    "homoglyphs": [
+      "＿"
+    ]
+  },
+  {
+    "character": "`",
+    "homoglyphs": [
+      "｀"
+    ]
+  },
+  {
+    "character": "a",
+    "homoglyphs": [
+      "ɑ",
+      "α",
+      "а",
+      "ａ"
+    ]
+  },
+  {
+    "character": "b",
+    "homoglyphs": [
+      "Ь",
+      "Ᏼ",
+      "ｂ"
+    ]
+  },
+  {
+    "character": "c",
+    "homoglyphs": [
+      "ϲ",
+      "с",
+      "ⅽ",
+      "ｃ"
+    ]
+  },
+  {
+    "character": "d",
+    "homoglyphs": [
+      "ď",
+      "ԁ",
+      "ⅾ",
+      "ｄ"
+    ]
+  },
+  {
+    "character": "e",
+    "homoglyphs": [
+      "ė",
+      "е",
+      "ｅ"
+    ]
+  },
+  {
+    "character": "f",
+    "homoglyphs": [
+      "ｆ"
+    ]
+  },
+  {
+    "character": "g",
+    "homoglyphs": [
+      "ɡ",
+      "ｇ"
+    ]
+  },
+  {
+    "character": "h",
+    "homoglyphs": [
+      "һ",
+      "ｈ"
+    ]
+  },
+  {
+    "character": "i",
+    "homoglyphs": [
+      "і",
+      "Ꭵ",
+      "ⅰ",
+      "ｉ"
+    ]
+  },
+  {
+    "character": "j",
+    "homoglyphs": [
+      "ϳ",
+      "ј",
+      "ｊ"
+    ]
+  },
+  {
+    "character": "k",
+    "homoglyphs": [
+      "Κ",
+      "κ",
+      "K",
+      "ｋ"
+    ]
+  },
+  {
+    "character": "l",
+    "homoglyphs": [
+      "ⅼ",
+      "ｌ"
+    ]
+  },
+  {
+    "character": "m",
+    "homoglyphs": [
+      "ⅿ",
+      "ｍ"
+    ]
+  },
+  {
+    "character": "n",
+    "homoglyphs": [
+      "ｎ"
+    ]
+  },
+  {
+    "character": "o",
+    "homoglyphs": [
+      "ο",
+      "о",
+      "ｏ"
+    ]
+  },
+  {
+    "character": "p",
+    "homoglyphs": [
+      "ρ",
+      "Р",
+      "р",
+      "Ꮲ"
+    ]
+  },
+  {
+    "character": "q",
+    "homoglyphs": [
+      "ｑ"
+    ]
+  },
+  {
+    "character": "r",
+    "homoglyphs": [
+      "ｒ"
+    ]
+  },
+  {
+    "character": "s",
+    "homoglyphs": [
+      "ѕ",
+      "Տ",
+      "ｓ"
+    ]
+  },
+  {
+    "character": "t",
+    "homoglyphs": [
+      "τ",
+      "ｔ"
+    ]
+  },
+  {
+    "character": "u",
+    "homoglyphs": [
+      "υ",
+      "ｕ"
+    ]
+  },
+  {
+    "character": "v",
+    "homoglyphs": [
+      "ν",
+      "ѵ",
+      "ⅴ",
+      "ｖ"
+    ]
+  },
+  {
+    "character": "w",
+    "homoglyphs": [
+      "ѡ",
+      "ｗ"
+    ]
+  },
+  {
+    "character": "x",
+    "homoglyphs": [
+      "х",
+      "ⅹ",
+      "ｘ"
+    ]
+  },
+  {
+    "character": "y",
+    "homoglyphs": [
+      "ʏ",
+      "γ",
+      "у",
+      "ｙ"
+    ]
+  },
+  {
+    "character": "z",
+    "homoglyphs": [
+      "ｚ"
+    ]
+  },
+  {
+    "character": "{",
+    "homoglyphs": [
+      "｛"
+    ]
+  },
+  {
+    "character": "|",
+    "homoglyphs": [
+      "ǀ",
+      "ا",
+      "｜"
+    ]
+  },
+  {
+    "character": "}",
+    "homoglyphs": [
+      "｝"
+    ]
+  },
+  {
+    "character": "~",
+    "homoglyphs": [
+      "⁓",
+      "～"
+    ]
+  },
+  {
+    "character": "ß",
+    "homoglyphs": [
+      "ӧ"
+    ]
+  },
+  {
+    "character": "ä",
+    "homoglyphs": [
+      "Ä",
+      "Ӓ"
+    ]
+  },
+  {
+    "character": "ö",
+    "homoglyphs": [
+      "Ö",
+      "Ӧ"
+    ]
+  },
+  {
+    "character": "A",
+    "homoglyphs": [
+      "Ꭺ",
+      "Ａ"
+    ]
+  },
+  {
+    "character": "B",
+    "homoglyphs": [
+      "Β",
+      "В",
+      "Ᏼ"
+    ]
+  },
+  {
+    "character": "C",
+    "homoglyphs": [
+      "Ϲ",
+      "С",
+      "Ꮯ",
+      "Ⅽ"
+    ]
+  },
+  {
+    "character": "D",
+    "homoglyphs": [
+      "Ꭰ",
+      "Ⅾ"
+    ]
+  },
+  {
+    "character": "E",
+    "homoglyphs": [
+      "Ė",
+      "Ę",
+      "Ε",
+      "Е"
+    ]
+  },
+  {
+    "character": "F",
+    "homoglyphs": [
+      "Ϝ"
+    ]
+  },
+  {
+    "character": "G",
+    "homoglyphs": [
+      "ɢ",
+      "Ԍ",
+      "Ꮐ"
+    ]
+  },
+  {
+    "character": "H",
+    "homoglyphs": [
+      "ʜ",
+      "Η",
+      "Н",
+      "Ꮋ"
+    ]
+  },
+  {
+    "character": "I",
+    "homoglyphs": [
+      "Ι",
+      "І",
+      "Ⅰ"
+    ]
+  },
+  {
+    "character": "J",
+    "homoglyphs": [
+      "Ј",
+      "Ꭻ"
+    ]
+  },
+  {
+    "character": "K",
+    "homoglyphs": [
+      "Κ",
+      "К",
+      "Ꮶ",
+      "K"
+    ]
+  },
+  {
+    "character": "L",
+    "homoglyphs": [
+      "Ꮮ",
+      "Ⅼ"
+    ]
+  },
+  {
+    "character": "M",
+    "homoglyphs": [
+      "Μ",
+      "Ϻ",
+      "М",
+      "Ꮇ",
+      "ᛖ",
+      "Ⅿ"
+    ]
+  },
+  {
+    "character": "N",
+    "homoglyphs": [
+      "ɴ",
+      "Ν"
+    ]
+  },
+  {
+    "character": "O",
+    "homoglyphs": [
+      "Ο",
+      "О",
+      "Օ"
+    ]
+  },
+  {
+    "character": "P",
+    "homoglyphs": [
+      "Ρ",
+      "Р"
+    ]
+  },
+  {
+    "character": "Q",
+    "homoglyphs": [
+      "Ⴍ"
+    ]
+  },
+  {
+    "character": "R",
+    "homoglyphs": [
+      "ʀ",
+      "Ꮢ"
+    ]
+  },
+  {
+    "character": "S",
+    "homoglyphs": [
+      "Ѕ",
+      "Տ",
+      "Ⴝ",
+      "Ꮪ"
+    ]
+  },
+  {
+    "character": "T",
+    "homoglyphs": [
+      "Τ",
+      "Т",
+      "Ꭲ"
+    ]
+  },
+  {
+    "character": "U",
+    "homoglyphs": [
+      "Ա",
+      "Ս",
+      "⋃"
+    ]
+  },
+  {
+    "character": "V",
+    "homoglyphs": [
+      "Ѵ",
+      "Ꮩ",
+      "Ⅴ"
+    ]
+  },
+  {
+    "character": "W",
+    "homoglyphs": [
+      "Ꮃ"
+    ]
+  },
+  {
+    "character": "X",
+    "homoglyphs": [
+      "Χ",
+      "χ",
+      "Х",
+      "Ⅹ"
+    ]
+  },
+  {
+    "character": "Y",
+    "homoglyphs": [
+      "Υ",
+      "Ү"
+    ]
+  },
+  {
+    "character": "Z",
+    "homoglyphs": [
+      "Ζ",
+      "Ꮓ"
+    ]
+  }
+]
